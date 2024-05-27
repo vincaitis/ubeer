bd4ae4f907d512b749b7666629ee23c52ec2b925
vincvincvincvinc <152969599+vincvincvincvinc@users.noreply.github.com>
2024-05-27 15:14:47 +0200

    Initial commit

---
+++ src/main/java/com/ubeer/ubeer/UbeerApplication.java
@@ -0,0 +1,13 @@
+package com.ubeer.ubeer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UbeerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UbeerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ubeer/ubeer/UbeerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ubeer.ubeer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UbeerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
