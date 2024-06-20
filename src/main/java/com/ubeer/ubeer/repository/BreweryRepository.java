316636377a1d7bb81a193a02aceac3189bdd6947
vincvincvincvinc <152969599+vincvincvincvinc@users.noreply.github.com>
2024-06-20 11:58:45 +0200

    Brewery with images 2

--- src/main/java/com/ubeer/ubeer/repository/BreweryRepository.java
+++ src/main/java/com/ubeer/ubeer/repository/BreweryRepository.java
@@ -2,8 +2,13 @@ package com.ubeer.ubeer.repository;
 
 import com.ubeer.ubeer.entity.BreweryEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+import java.util.List;
 
 @Repository
 public interface BreweryRepository extends JpaRepository<BreweryEntity, Long> {
+
+    @Query("SELECT b FROM BreweryEntity b LEFT JOIN FETCH b.images")
+    List<BreweryEntity> findAllWithImages();
 }
